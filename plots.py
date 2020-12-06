9976f75fa70ecc4cc20994ac8585d8e886fb4f84
Anita Dürr <anita.d.durr@gmail.com>
2020-12-06 12:20:33 +0100

    tight layout for avg plots : x label not cut anymore

--- plots.py
+++ plots.py
@@ -21,11 +21,11 @@ def sample_S_approx(datas, betas, lambda_, n_iter, verbose=False):
     list_lambdas = []
     list_datas = []
 
-    
+
     starting_state = np.zeros(datas[0].N)
-    
+
     for k in range(len(datas)):
-        
+
         starting_state[ datas[k].v == np.max(datas[k].v)] = 1
         S_approx = simulated_annealing(starting_state, betas, n_iter, lambda_, datas[k], verbose)
 
@@ -50,19 +50,20 @@ def plot_avg_lambda(G, lambdas, betas, n_iter, nb_instances, verbose=False):
     E = [avg(datas, betas, lambda_, n_iter, verbose) for lambda_ in lambdas]
     E = np.array(E)
 
-    fig_obj, ax_obj = plt.subplots(figsize=(1 + len(lambdas), 4))
+    fig_obj, ax_obj = plt.subplots(figsize=(1 + len(lambdas), 6), constrained_layout=True)
     ax_obj.plot(lambdas, E[:, 0], '+', ls=':')
     ax_obj.set_xlabel("Lambda", fontsize=25)
-    ax_obj.set_ylabel("Average max obj", fontsize=25)
+    ax_obj.set_ylabel("Average maximum of f", fontsize=25)
     ax_obj.tick_params(labelsize=17)
 
-    fig_size, ax_size = plt.subplots(figsize=(1 + len(lambdas), 4))
+    fig_size, ax_size = plt.subplots(figsize=(1 + len(lambdas), 6), constrained_layout=True)
     ax_size.plot(lambdas, E[:, 1], '+', ls=':')
     ax_size.set_xlabel("Lambda", fontsize=25)
-    ax_size.set_ylabel("Average maxi size", fontsize=25)
+    ax_size.set_ylabel("Average size of S maximizing", fontsize=25)
     ax_size.tick_params(labelsize=17)
-    
+
     data_name = "G1" if isinstance(datas[0], G1) else "G2"
-    
+
+    plt.tight_layout()
     fig_obj.savefig('plots\\avg_obj_{}.pdf'.format(data_name))
     fig_size.savefig('plots\\avg_size_{}.pdf'.format(data_name))
